a983d1e521439142253a07e645dabf64d97617b3
Atsushi Masumori <atsmsmr@EUKELADE.local>
2017-07-30 11:58:24 +0900

    Revise filepath in testDLL.cpp

--- FavLibrary/buildTest/testDLL.cpp
+++ FavLibrary/buildTest/testDLL.cpp
@@ -5,10 +5,8 @@
 int main(int argc, const char * argv[]) {
     
     FavLibrary::Fav fav_test;
-    fav_test.read("/Users/atsmsmr/Downloads/sample_validation_error.fav");
-    //    fav_test.read("/Users/atsmsmr/Downloads/testomato200_base64.xml");
-    fav_test.write("/Users/atsmsmr/Downloads/testomato200_4.xml", FavLibrary::CompressionMode::base64);
-    //
-    //                return 0;
-    //                }
+    fav_test.read("../../../Sample\ FAVs/ChessKing_Color_reso1_v1.fav");
+    fav_test.write("./testLib.fav", FavLibrary::CompressionMode::base64);
+    return 0;
+
 }
