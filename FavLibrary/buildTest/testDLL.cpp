d8da6fb85c220d3b504a651d8fbe43f2cc62598e
Atsushi Masumori <ats.msmr@gmail.com>
2018-02-15 00:43:23 +0900

    Update testDLL.cpp
    サンプルファイルのディレクトリをSample_Favsに修正

--- FavLibrary/buildTest/testDLL.cpp
+++ FavLibrary/buildTest/testDLL.cpp
@@ -5,7 +5,7 @@
 int main(int argc, const char * argv[]) {
     
     FavLibrary::Fav fav_test;
-    fav_test.read("../../../Sample\ FAVs/ChessKing_Color_reso1_v1.fav");
+    fav_test.read("../../../Sample_FAVs/ChessKing_Color_reso1_v1.fav");
     fav_test.write("./testLib.fav", FavLibrary::CompressionMode::base64);
     return 0;
 
